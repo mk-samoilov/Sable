2030670e49ef4d23391a4f0c233c5f7cef9cd04c
mk-samoilov <me@mk-samoilov.ru>
2026-06-27 00:08:46 +0300

    extract quad creation from scene to main

--- engine/src/core/scene.cpp
+++ engine/src/core/scene.cpp
@@ -1,7 +1,6 @@
 #include "sableEng/core/scene.h"
 #include "sableEng/engine.h"
 #include "sableEng/gfx/vkbase.h"
-#include "sableEng/gameobjects/objects/sprite.h"
 
 namespace Core
 {
@@ -9,20 +8,20 @@ namespace Core
     {
         Gfx::Pipeline::GetInstance()->BuildStandardMaterials();
 
-        std::vector<Gfx::Vertex> vertices = {
-            {{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-            {{ 0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
-            {{ 0.5f,  0.5f}, {0.0f, 0.0f, 1.0f}},
-            {{-0.5f,  0.5f}, {1.0f, 1.0f, 1.0f}}
-        };
-        std::vector<uint16_t> indices = { 0, 1, 2, 2, 3, 0 };
-        Gfx::Mesh::GetInstance()->CreateMesh("quad", vertices, indices);
+        // std::vector<Gfx::Vertex> vertices = {
+        //     {{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+        //     {{ 0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
+        //     {{ 0.5f,  0.5f}, {0.0f, 0.0f, 1.0f}},
+        //     {{-0.5f,  0.5f}, {1.0f, 1.0f, 1.0f}}
+        // };
+        // std::vector<uint16_t> indices = { 0, 1, 2, 2, 3, 0 };
+        // Gfx::Mesh::GetInstance()->CreateMesh("quad", vertices, indices);
 
-        GameObjects::Info info;
-        info.Material = "sprite";
-        info.Mesh = "quad";
-        info.ParsedID = "quad_0";
-        Keeper.Create<GameObjects::Sprite>(new GameObjects::Sprite(info));
+        // GameObjects::Info info;
+        // info.Material = "sprite";
+        // info.Mesh = "quad";
+        // info.ParsedID = "quad_0";
+        // Keeper.Create<GameObjects::Sprite>(new GameObjects::Sprite(info));
 
         StagesManager.Populate(CurrentScene, [](GameObjects::Type type) {
             return type == GameObjects::CAMERA;
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
 #include <sableEng/engine.h>
+#include <sableEng/core/scene.h>
+#include <sableEng/gameobjects/gameobjects.h>
+#include <sableEng/gameobjects/objects/sprite.h>
+#include <sableEng/gfx/vkmesh.h>
+
+#include <vector>
 
 void mainLoop(float dt) {
 
@@ -7,6 +13,24 @@ void mainLoop(float dt) {
 int main() {
     try {
         Engine::GetInstance()->Init();
+
+        GameObjects::Keeper* objectsKeeper = Core::Scene::GetInstance()->GetKeeper();
+
+        std::vector<Gfx::Vertex> vertices = {
+            {{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+            {{ 0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
+            {{ 0.5f,  0.5f}, {0.0f, 0.0f, 1.0f}},
+            {{-0.5f,  0.5f}, {1.0f, 1.0f, 1.0f}}
+        };
+        std::vector<uint16_t> indices = { 0, 1, 2, 2, 3, 0 };
+        Gfx::Mesh::GetInstance()->CreateMesh("quad", vertices, indices);
+
+        GameObjects::Info info;
+        info.Material = "sprite";
+        info.Mesh = "quad";
+        info.ParsedID = "quad_0";
+        objectsKeeper->Create<GameObjects::Sprite>(new GameObjects::Sprite(info));
+
         Engine::GetInstance()->Run(mainLoop);
     } catch (const std::exception& e) { std::cerr << e.what() << "\n"; return 1; }
 }
